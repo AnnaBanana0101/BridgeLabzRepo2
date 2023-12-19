86d2e4e7a5d1df1e150b4b7ee5ad5dd8469b6af1
AnnaBanana0101 <77004422+AnnaBanana0101@users.noreply.github.com>
2023-12-19 19:23:43 +0530

    Implemented UC3

---
+++ src/CompareDist.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class CompareDist {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("x1:");
+        double x1 = sc.nextDouble();
+
+        System.out.println("y1:");
+        double y1 = sc.nextDouble();
+
+        System.out.println("x2:");
+        double x2 = sc.nextDouble();
+
+        System.out.println("y2:");
+        double y2 = sc.nextDouble();
+
+        System.out.println("a1:");
+        double a1 = sc.nextDouble();
+
+        System.out.println("b1:");
+        double b1 = sc.nextDouble();
+
+        System.out.println("a2:");
+        double a2 = sc.nextDouble();
+
+        System.out.println("b2:");
+        double b2 = sc.nextDouble();
+
+        double distance1, distance2;
+
+        double sq1 = Math.pow((x1 - x2), 2);
+        double sq2 = Math.pow((y1 - y2), 2);
+
+        double sq3 = Math.pow((a1 - a2), 2);
+        double sq4 = Math.pow((b1 - b2), 2);
+
+        distance1 = Math.sqrt(sq1 + sq2);
+        distance2 = Math.sqrt(sq3 + sq4);
+
+        Double dist1 = new Double(distance1);
+        Double dist2 = new Double(distance2);
+        int compare = dist1.compareTo(dist2);
+
+        if (compare == 0)
+            System.out.println("Equal");
+        else if (compare < 0)
+            System.out.println("Line 1 is smaller");
+        else
+            System.out.println("Line 2 is smaller");
+
+        System.out.println("Distance 1: " + distance1);
+        System.out.println("Distance 2: " + distance2);
+
+    }
+}
